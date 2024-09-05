fffdd0a0645eb29a7a13233b7d06e13a7bf6c301
Monica Solines <160787107+monicasolines@users.noreply.github.com>
2024-09-05 15:29:12 +0000

    feat: fetch get, post, delete

--- src/js/component/TodoList.jsx
+++ src/js/component/TodoList.jsx
@@ -4,22 +4,39 @@ export const TodoList = () => {
     const [todo, setTodo] = useState('');
     const [todos, setTodos] = useState([])
     const [hovered, setHovered] = useState(null);
-    const host = 'https://playground.4geeks.com/todo/users'
+    const host = 'https://playground.4geeks.com/todo'
 
 
-    const addTodo = () => {
-        setTodos([...todos, todo]);
-        setTodo('')
-    }
-
-    const eliminarLi = (index) => {
-        setTodos(todos.filter((element, i) => i !== index));
-    }
-
-    const getTodos = async () => {
-        const uri = `${host}/monisolines`;
+    const addTodo = async () => {
+        const dataToSend = {
+            label: todo,
+            is_done: false,
+        }
+        const uri = `${host}/todos/monisolines`;
         const options = {
-            method: 'GET',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(dataToSend),
+        }
+
+        const response = await fetch(uri, options);
+
+        if (!response.ok) {
+            console.log('Error:', response.status, response.statusText);
+            return
+        }
+        setTodo('');
+        getTodos();
+        // setTodos([...todos, todo]);
+
+    }
+
+    const eliminarLi = async (id) => {
+        const uri = `${host}/todos/${id}`;
+        const options = {
+            method: 'DELETE',
         }
 
         const response = await fetch(uri, options);
@@ -29,14 +46,37 @@ export const TodoList = () => {
             return
         }
 
+        getTodos();
+
+        // setTodos(todos.filter((element, i) => i !== index));
+    }
+
+    const getTodos = async () => {
+        const uri = `${host}/users/monisoline`;
+        const options = {
+            method: 'GET',
+        }
+
+        const response = await fetch(uri, options);
+
+        if (!response.ok) {
+            console.log('Error:', response.status, response.statusText);
+            if (response.status === 404) {
+                // podriamos crear el usuario, llamando a funcion ej: createUser
+            }
+            return
+        }
+
         const data = await response.json();
-        console.log('Error:', response.status, response.statusText);
+        console.log(data);
 
         setTodos(data.todos)
 
     }
 
-    useEffect (() => {
+
+
+    useEffect(() => {
         getTodos();
     }, []);
 
@@ -60,17 +100,17 @@ export const TodoList = () => {
                     {todos.map((element, index) => (
                         <li
                             className="list-group-item"
-                            key={index}
+                            key={element.id}
                             onMouseOver={() => setHovered(index)}
                             onMouseOut={() => setHovered(null)}
                             style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', fontFamily: 'Raleway, sans-serif', fontWeight: 100 }}
                         >
-                            <span> {element} </span>
+                            <span> {element.label} </span>
                             {hovered === index && (
                                 <i
                                     className="far fa-trash-alt d-flex justify-content-end text-end align-content-end"
                                     style={{ marginLeft: '10px', cursor: 'pointer' }}
-                                    onClick={() => eliminarLi(index)}
+                                    onClick={() => eliminarLi(element.id)}
                                 ></i>
                             )}
                         </li>
